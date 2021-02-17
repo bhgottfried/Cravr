f31f71bf84cc74f79cb02adce74accbc2933819a
Eli Talcott <eli.talcott@gmail.com>
2021-02-17 18:39:52 -0500

    Resolve merge conflicts

--- backend/flaskr/app.py
+++ backend/flaskr/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -9,3 +9,20 @@ CORS(app)
 @app.route('/hello')
 def say_hello_world():
     return {'result': "Hello world!"}
+
+@app.route('/login', methods=["POST"])
+def login():
+    user, password = request.json.split('\n')
+    match = True # Check if matches database
+    return {'result': "/Home" if match else "/"}
+
+@app.route('/restaurants', methods=["GET"])
+def restaurants():
+    # Parse the user's restaurant request and get restaurants from Yelp
+    return {'result': "TODO"}
+
+
+@app.route('/rated', methods=["POST"])
+def rated():
+    # Apply the user's rating to their profile and the restaurant's
+    return {'result': "TODO"}
