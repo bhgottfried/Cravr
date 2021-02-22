9919acd3f6b7e45b99977b584ba5191a89060bd6
Ben Gottfried <35643275+bhgottfried@users.noreply.github.com>
2021-02-22 09:39:06 -0500

    Activate pylint github action for backend codebase (#13)
    * Testing pylint action
    
    * Fix path to requirements.txt
    
    * Remove unnecessary requirements
    
    * Add py docstrings
    
    * Finalize linting
    
    * Change linting to work when pushing to master

--- backend/flaskr/app.py
+++ backend/flaskr/app.py
@@ -1,3 +1,5 @@
+"""Main backend routing file to respond to frontend API requests"""
+
 from flask import Flask, request
 from flask_cors import CORS
 from backend.flaskr.authentication_utils import authenticate_user
@@ -5,23 +7,22 @@ from backend.flaskr.authentication_utils import authenticate_user
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/hello')
-def say_hello_world():
-    return {'result': "Hello world!"}
 
 @app.route('/login', methods=["POST"])
 def login():
+    """Attempt to login the user with the provided credentials"""
     user, password = request.json.split('\n')
     match = authenticate_user(app, user, password)
     return {'result': "/Home" if match else "/"}
 
+
 @app.route('/restaurants', methods=["GET"])
 def restaurants():
-    # Parse the user's restaurant request and get restaurants from Yelp
+    """Parse the user's restaurant request and get restaurants from Yelp"""
     return {'result': "TODO"}
 
 
 @app.route('/rated', methods=["POST"])
 def rated():
-    # Apply the user's rating to their profile and the restaurant's
+    """Apply the user's rating to their profile and the restaurant's"""
     return {'result': "TODO"}
--- backend/setup.py
+++ backend/setup.py
@@ -1,3 +1,5 @@
+"""Setup file used for Dependabot"""
+
 from setuptools import setup
 
 setup(
