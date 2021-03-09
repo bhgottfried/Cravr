97b2ddd91c3788dd610d2bb30484d1c787cb36b8
Ben Gottfried <bhgottfried@gmail.com>
2021-03-08 20:48:26 -0500

    Limit query distance to what Yelp can support

--- frontend/src/FindQuiz.js
+++ frontend/src/FindQuiz.js
@@ -149,7 +149,7 @@ class FindQuizContainer extends React.Component {
                     <br />
                     <label id="q3">
                         3. Maxmimum Distance (mi.)&emsp;
-                        <input type="number" defaultValue="1" min="1" value={this.state.q2.value} name="q3"
+                        <input type="number" defaultValue="1" min="1" max="24" value={this.state.q2.value} name="q3"
                                 required onChange={this.handleChange}>
                         </input>
                     </label>
