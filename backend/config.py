ed3c9c009a829b59b8ce3f9d25271000e9837eac
Eli Talcott <eli.talcott@gmail.com>
2021-03-29 20:21:58 -0400

    Added mysql service in docker compose file, edited database config, and added mysql schema

--- backend/config.py
+++ backend/config.py
@@ -5,9 +5,9 @@ class DatabaseConfig:  # pylint: disable=too-few-public-methods
     # Database
     MYSQL_DATABASE_HOST = "localhost"
     MYSQL_DATABASE_PORT = 3306
-    MYSQL_DATABASE_USER = "root"
+    MYSQL_DATABASE_USER = "backend"
     MYSQL_DATABASE_PASSWORD = "ece49595bois!"
-    MYSQL_DATABASE_DB = "authdb"
+    MYSQL_DATABASE_DB = "cravr"
     MYSQL_DATABASE_CHARSET = "utf8"
 
 class YelpConfig:  # pylint: disable=too-few-public-methods
